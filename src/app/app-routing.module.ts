5487b531afe387cdcd7f862c324445754b9b461e
Chamud-Sachintha <chamudsachintha999@gmail.com>
2023-09-29 20:21:42 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WebLayoutComponent } from './layouts/web-layout/web-layout.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: WebLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import("./modules/web-module/web-module.module").then(m => m.WebModuleModule)
+      }
+    ]
+  },
+
+  {
+    path: 'auth',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import("./modules/auth-module/auth-module.module").then(m => m.AuthModuleModule)
+      }
+    ]
+  },
+
+  {
+    path: 'app',
+    component: DashboardLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import("./modules/dashboard-module/dashboard-module.module").then(m => m.DashboardModuleModule)
+      }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
